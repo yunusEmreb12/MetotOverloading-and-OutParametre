20d341aba4dc11f3ccbb77dee3aaf6fda9fa9e99
Emre Basarir <yunu_emre_kardes@outlook.com>
2022-09-26 17:51:18 +0300

    Metot Overloading and out parametre

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace metot_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out parametreler
+            string sayi = "999";
+            int outSayi;
+
+            bool sonuc = int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {   
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            // Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Zikriye"," Ürkmez");
+
+            // Metot İmzası
+            // metotAdı + parametre sayisi + parametre
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        } 
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+(C# Metot Overloading ve Out Parametre kullanımı)[https://app.patika.dev/courses/csharp-101/2-metot-overload-nedir-ve-out-kullanimi]
